36bd1e2910093f9446cccd853ad130186c70f3aa
Saroeut Sim <saroeuts123@gmail.com>
2024-09-09 18:14:36 -0700

    first commit

---
+++ Todo25/src/App.tsx
@@ -0,0 +1,13 @@
+import ToDoList from "./components/ToDoList"
+
+
+const App = () => {
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
---
+++ Todo25/src/components/ToDoList.tsx
@@ -0,0 +1,40 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+interface ToDo{
+    id: number
+    title: string
+    completed: boolean
+}
+
+
+const ToDoList = () => {
+
+// Lets create a fetch data function to help us fetch our data with axios
+const [todos, setTodos] = useState<ToDo[]>([])
+const [error, setError] = useState()
+
+
+const fetchData = () => 
+{
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+    .then(res => setTodos (res.data))
+    .catch(error => setError(error));
+}
+
+useEffect(() => {
+ fetchData();
+}, [])
+
+
+
+  return (
+    <>
+        {todos.map((todo) => {
+            <li>{todo.title}</li>
+        })}
+    </>
+  )
+}
+
+export default ToDoList
